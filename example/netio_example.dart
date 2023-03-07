1cb28d1a0e84213467d00eb4fadcb87edc04dadd
Tushar Ghige <dev.tushar.g@gmail.com>
2023-03-07 10:44:35 +0530

    New: Added simple dart example

--- example/netio_example.dart
+++ example/netio_example.dart
@@ -1,1 +1,14 @@
-void main() {}
+import 'package:netio/netio.dart';
+
+void main() async {
+  final netio = Netio();
+
+  final resp = await netio.get(
+    "https://api.ipify.org",
+    queryParameters: {"format": "json"},
+  );
+
+  final ip = resp.body['ip'];
+
+  print("[Your IP] $ip");
+}
